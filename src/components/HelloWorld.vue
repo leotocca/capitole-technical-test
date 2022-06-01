19c426690c4f7bc475f3f78ca1991f7eda71b5dc
Leo Toccaceli <leotoccaceli@gmail.com>
2022-06-01 15:14:44 +0200

    Basic project setup

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,19 @@
-<script setup>
-import { ref } from 'vue'
-
-defineProps({
-  msg: String
-})
-
-const count = ref(0)
-</script>
-
 <template>
-  <h1>{{ msg }}</h1>
-
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VS Code</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Documentation
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+	<div class="w-full h-screen flex flex-col items-center justify-center">
+		<h1 class="text-sm font-bold underline text-red-700">
+			{{ msg }}
+		</h1>
+	</div>
 </template>
-
+<script>
+export default {
+	props: {
+		msg: String,
+	},
+};
+</script>
 <style scoped>
 a {
-  color: #42b983;
+	color: #42b983;
 }
 </style>
