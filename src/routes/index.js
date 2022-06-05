a8868623a78a970f1e9f54dab67c0849c20c58ed
Leo Toccaceli <leotoccaceli@gmail.com>
2022-06-05 22:58:30 +0200

    Created Search page, finished api call to make a search

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,14 @@ import Home from "../pages/Home.vue";
 import About from "../pages/About.vue";
 import Category from "../pages/Category.vue";
 import MovieDetail from "../pages/MovieDetail.vue";
+import Search from "../pages/Search.vue";
 
 const routes = [
 	{ path: "/", component: Home },
 	{ path: "/about", component: About },
 	{ path: "/category/:category", component: Category },
 	{ path: "/movie-detail/:id", component: MovieDetail },
+	{ path: "/search", component: Search },
 ];
 
 const router = createRouter({
